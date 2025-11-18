cb25cf5208b3678951811e2d19c54aca7ba0e3c7
jtkIII <me@jtk3.com>
2025-11-18 08:55:52 -0600

    Jinja!

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # newz.day
 
-### Efficient, Propaganda Resistant News.  Daily.
+## Efficient, Propaganda Resistant News.  Daily
 
+### Start the server
 
-##### Can you get more efficient than HTMX + FastAPI?  Well and an great db of course.
-
-
-
-## Start the server:
 `API_KEY=a_super_secret_api_key uvicorn app.main:app --reload`
 
-## Call an endpoint
+#### Call an endpoint
+
 `curl -H "x-api-key:a_super_secret_api_key" http://127.0.0.1:8000/api/v1/newz/`
+
+--------------------------------------------------------------------------
+
+#### You'll need to make a .env file that has contents similar to
+
+API_KEY=some_super_secret_api_key
+DEBUG=true
+HOST=127.0.0.1
+PORT=8000
+ALLOWED_IPS=127.0.0.1,10.0.0.0/8
+CACHE_TTL_SECONDS=5
--- app/routes/latest.py
+++ app/routes/latest.py
@@ -1,15 +1,19 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Request  # , Form
 
-from app.core.auth import verify_api_key
+# from app.core.auth import verify_api_key - not needed for latest it's public
 from app.data.models import APIResponse, API_Error
 from app.services.newz import get_latest
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-router = APIRouter(dependencies=[Depends(verify_api_key)])
+router = APIRouter()
 newz = get_latest()
 
+templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/", summary="Get the latest news", response_model=APIResponse | API_Error)
-def latest_newz():
+
+@router.get("/", summary="Get the latest news", response_class=HTMLResponse)
+def latest_newz(request: Request):
     if not newz:
         return API_Error(message="Latest news not available", code=404)
     latest = APIResponse(
@@ -20,4 +24,7 @@ def latest_newz():
         message="Latest news fetched successfully",
     )
 
-    return latest
+    # return latest
+    return templates.TemplateResponse(
+        "news.html", {"request": request, "items": latest.result}
+    )
